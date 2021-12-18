f042bfffa36d945ad442f415b937c849df903fdf
Filip Dura <kili98@wp.pl>
2021-12-18 16:20:22 +0100

    zmiana nazw klas itd.

--- Projekt/Kalkulator.xaml.cs
+++ Projekt/Kalkulator.xaml.cs
@@ -18,9 +18,9 @@ namespace Projekt
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Calculator : Window
     {
-        public MainWindow()
+        public Calculator()
         {
             InitializeComponent();
         }
